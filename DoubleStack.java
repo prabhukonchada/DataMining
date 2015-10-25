ac6d2ac7dcfe06c469b61838d8043d6d41fa76ca
Prabhu Konchada <prabhukonchada@summerinterns>
2015-10-25 13:01:55 +0530

    Push implemented on the double stack

--- DoubleStack.java
+++ DoubleStack.java
@@ -5,10 +5,10 @@ import java.util.*;
 
 public class DoubleStack
 {
-  ArrayList doubleStack;
+  int  doubleStack[];
   Scanner inputScanner;
-  int topOfStackOne=-1,topOfStackTwo=-1;
-  int baseOfStackOne=-1,baseOfStackTwo=-1;
+  int topOfStackOne=-1,topOfStackTwo,sizeOfDoubleStack;
+  int baseOfStackOne=-1,baseOfStackTwo;
 
   public static void main(String args[])
   {
@@ -16,21 +16,27 @@ public class DoubleStack
     myDoubleStack.inputScanner = new Scanner(System.in);
     System.out.println("Enter the size of double stack");
 
-    int size = myDoubleStack.inputScanner.nextInt();
-    myDoubleStack.doubleStack = new ArrayList(size);
+    myDoubleStack.sizeOfDoubleStack = myDoubleStack.inputScanner.nextInt();
+    myDoubleStack.topOfStackTwo=(myDoubleStack.sizeOfDoubleStack);
 
-    while(1)
+    myDoubleStack.doubleStack = new int[myDoubleStack.sizeOfDoubleStack];
+
+  /*  for(int i=0;i<myDoubleStack.sizeOfDoubleStack;i++)
+    {
+      myDoubleStack.doubleStack.add(0);
+    }*/
+
+    while(true)
     {
 
-      System.out.println("Enter Stack Number\n 1: StackOne \n 2:StackTwo")
+      System.out.println("Enter Stack Number\n 1: StackOne \n 2:StackTwo");
       int stackNumber = myDoubleStack.inputScanner.nextInt();
 
       // Initialize the index values of the bases of the stack
 
       myDoubleStack.baseOfStackOne=0;
-      myDoubleStack.baseOfStackTwo=myDoubleStack.topOfStackOne;
-
-      //TODO : Display Menu To Either Push / Pop / Display / Exit
+      // System.out.println(myDoubleStack.sizeOfDoubleStack);
+      // //TODO : Display Menu To Either Push / Pop / Display / Exit
       System.out.println("1. Push Into The Stack");
       System.out.println("2. Pop From The Stack");
       System.out.println("3. Display Elements In The Stack");
@@ -40,15 +46,17 @@ public class DoubleStack
       switch(selectedChoice)
       {
         case 1:
-          push(stackNumber);
+          System.out.println("Enter an element to push");
+          int element=myDoubleStack.inputScanner.nextInt();
+          myDoubleStack.push(stackNumber,myDoubleStack,element);
           break;
 
         case 2:
-          pop(stackNumber);
+          myDoubleStack.pop(stackNumber);
           break;
 
         case 3:
-          printStackElements(stackNumber);
+          myDoubleStack.printStackElements(stackNumber,myDoubleStack);
           break;
 
         case 4:
@@ -62,20 +70,62 @@ public class DoubleStack
 
 
   // TODO : Implement Push Functionality for the double stack
-  private void push()
+  private void push(int stackNumber,DoubleStack doubleStackObject,int element)
   {
+      int topIndexValue;
+      if(stackNumber==1)
+      {
+        topIndexValue = doubleStackObject.topOfStackOne;
 
+        if(topIndexValue >= doubleStackObject.topOfStackTwo-1)
+        {
+          System.out.println("Stack One Overflowed");
+          return;
+        }
+        doubleStackObject.topOfStackOne=++topIndexValue;
+      //  System.out.println(doubleStackObject.topOfStackOne);
+      }
+      else
+      {
+        topIndexValue = doubleStackObject.topOfStackTwo;
+        if(topIndexValue <= (doubleStackObject.topOfStackOne)+1)
+        {
+          System.out.println("Stack Two Overflowed");
+          return;
+        }
+
+        doubleStackObject.topOfStackTwo=--topIndexValue;
+        System.out.println("topOfStackTwo "+doubleStackObject.topOfStackTwo+"topIndexValue"+topIndexValue);
+
+      }
+      doubleStackObject.doubleStack[topIndexValue]=element;
   }
 
   // TODO : Implement Pop Functionality for the double stack
-  private void pop()
+  private void pop(int stackNumber)
   {
 
   }
 
   // TODO : Pretty Print the list of elements in the respective selected stack
-  private void printStackElements()
+  private void printStackElements(int stackNumber,DoubleStack doubleStackObject)
   {
+    //System.out.println(doubleStackObject.doubleStack.get(0));
 
+      if(stackNumber == 1)
+      {
+        for(int i=doubleStackObject.topOfStackOne;i>=0;i--)
+        {
+          System.out.println(doubleStackObject.doubleStack[i]);
+        }
+      }
+      else if(stackNumber == 2)
+      {
+        System.out.println("value of top stack2"+doubleStackObject.topOfStackTwo);
+        for(int i=doubleStackObject.topOfStackTwo;i<=(doubleStackObject.sizeOfDoubleStack)-1;i++)
+        {
+          System.out.println(doubleStackObject.doubleStack[i]);
+        }
+      }
   }
 }
